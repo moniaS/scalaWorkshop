0ffb727de8df038be7b59a8e25e651a080369101
mstobieniecka <mstobieniecka1@gmail.com>
2018-04-21 20:50:47 +0200

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "scalaWorkshop"
+
+version := "0.1"
+
+scalaVersion := "2.12.5"
---
+++ src/main/scala/ApplicationStartup.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by mstobieniecka on 2018-04-21.
+  */
+object ApplicationStartup {
+  def main(args: Array[String]): Unit = {
+    print("Application started")
+  }
+}
