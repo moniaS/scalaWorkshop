5de64dfbfe28fcdfbc25a3195dc6c089069a5a98
Anna Pietrzak <195680@edu.p.lodz.pl>
2018-04-24 23:06:54 +0200

    examples for closure

---
+++ src/main/scala/lab7/ClosureExample.scala
@@ -0,0 +1,58 @@
+import lab7.Foo
+import scala.collection.mutable.ArrayBuffer
+
+object ClosureExample extends App {
+
+
+  //closure example with Integer variables
+
+  var adultAge = 18
+  val isAdult = (age: Int) => age >= adultAge
+  println(isAdult(15))
+  println(isAdult(22))
+
+  def printResult(f: Int => Boolean, x: Int) {
+    println(f(x))
+  }
+  printResult(isAdult, 20)
+  adultAge = 21
+  printResult(isAdult, 20)
+
+
+
+  //closure example with String  variables
+
+  var name = "Mark"
+  val introduction = "My name is"
+  def greeting(intro: String) { println(intro + ' ' + name) }
+
+  val foo = new Foo
+  foo.exec(greeting, introduction)
+
+  name = "Eric"
+  foo.exec(greeting, introduction)
+
+
+
+  //closures can work with any data type, including collections
+
+  //closure example with ArrayBuffer
+
+  val basket = ArrayBuffer("Apple")
+  //function addToBasket can be passed to another function and still will have a reference to basket value
+  val addToBasket = (item: String) => {
+    basket += item
+    println("Items in the basket: " + basket.mkString(", "))
+  }
+
+  def goShopping(f: String => Unit, s: String) {
+    f(s)
+  }
+
+  goShopping(addToBasket, "Chocolate")
+  goShopping(addToBasket, "Orange")
+  goShopping(addToBasket, "Coca-cola")
+
+
+
+}
